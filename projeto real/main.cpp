0ecf254601b63acb1584ff93c5d1c05ceb6cde7c
Nathan Camargo <72906853+nathan-camargo@users.noreply.github.com>
2023-06-09 23:30:15 -0300

    Update main.cpp

--- projeto real/main.cpp
+++ projeto real/main.cpp
@@ -1,46 +1,28 @@
-#include <WiFi.h>  
-#include <HTTPClient.h>  
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const char* ssid = "vamos inserir o wifi";  
-const char* senha = "vamos inserir a senha do wifi";  
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const char* servidor_http = "servidor";  
-const int porta_http = 80;  
-const char* rota_http = "/enviar-dados";  
+const int pinoDisparo = 14;
+const int pinoEco = 12;
 
-const int pinoDisparo = 14;  
-const int pinoEco = 12;  
+#define VELOCIDADE_SOM 0.034
+#define ALTURA_LIXEIRA 50
+#define LIMITE_SUPERIOR 85
 
-#define VELOCIDADE_SOM 0.034  
-#define ALTURA_LIXEIRA 50  
+long duracao;
+int distanciaCm;
+int nivelLixo;
+int percentualNivelLixo;
 
-long duracao;  
-int distanciaCm;  
-int nivelLixo;  
-int percentualNivelLixo;  
+void setup() {
+  Serial.begin(115200);
+  lcd.begin(16, 2);
+  pinMode(pinoDisparo, OUTPUT);
+  pinMode(pinoEco, INPUT);
+}
 
-void configurar_wifi() {  
-  delay(10);  
-  WiFi.begin(ssid, senha);  
-
-  while (WiFi.status() != WL_CONNECTED) {  
-    delay(500);  
-    Serial.println("Conectando ao WiFi..");  
-  }  
-
-  Serial.println("WiFi conectado");  
-  Serial.println("Endereço IP: ");  
-  Serial.println(WiFi.localIP());  
-}  
-
-void setup() {  
-  Serial.begin(115200);  
-  configurar_wifi();  
-  pinMode(pinoDisparo, OUTPUT);   
-  pinMode(pinoEco, INPUT);   
-}  
-
-void loop() {  
+void loop() {
   digitalWrite(pinoDisparo, LOW);
   delayMicroseconds(2);
   digitalWrite(pinoDisparo, HIGH);
@@ -54,21 +36,26 @@ void loop() {
   nivelLixo = ALTURA_LIXEIRA - distanciaCm;
   percentualNivelLixo = (nivelLixo * 100) / ALTURA_LIXEIRA;
 
-  String url = String(servidor_http) + String(rota_http) + "?nivelLixo=" + String(nivelLixo) + "&percentualNivelLixo=" + String(percentualNivelLixo);  
+  lcd.clear();
 
-  HTTPClient http;  
-  http.begin(url);  
+  lcd.setCursor(0, 0);
+  lcd.print("Nível atual do");
+  
+  lcd.setCursor(0, 1);
 
-  int httpResponseCode = http.GET();  
+  if (percentualNivelLixo < LIMITE_SUPERIOR) {
+    lcd.print("lixo: ");
+    lcd.print(percentualNivelLixo);
+    lcd.print("%");
+    lcd.setCursor(0, 3);
+    lcd.print("Não precisa retirar ainda!");
+  } else {
+    lcd.print("lixo: ");
+    lcd.print(percentualNivelLixo);
+    lcd.print("%");
+    lcd.setCursor(0, 3);
+    lcd.print("Retire o lixo!");
+  }
 
-  if (httpResponseCode == HTTP_CODE_OK) {  
-    Serial.println("Dados enviados com sucesso para o servidor HTTP");  
-  } else {  
-    Serial.print("Falha ao enviar dados para o servidor HTTP. Código de resposta: ");  
-    Serial.println(httpResponseCode);  
-  }  
-
-  http.end();  
-
-  delay(5000); // Espere 5 segundos antes de enviar novamente
+  delay(5000);
 }
